41b1f225a2de373d7f55f99cc1f6fe65bd9ed784
Phan <phan.duc@futuresplatform.com>
2021-08-27 01:52:49 +0300

    fix: fixed issue: can not fetch phenomena list of Related content when Creating new phen ( Workaround)

--- src/index.js
+++ src/index.js
@@ -22,187 +22,337 @@ const ContentFilters = ({
   countShown = true,
   resetShown = true,
   manualFilterReset,
-  groups,
+  groupsProp,
   groupsLoading
 }) => {
-  const resetFilters = () => {
-    setSelectedTypes([])
-    setSelectedTimes(DEFAULT_TIMES)
-    setSelectedTags([])
-    setSelectedLanguage(SELECTED_LANGUAGE)
-    setSelectedGroup(PUBLIC_GROUP)
-  }
-  console.log('groups', groups)
-
-  const areFiltersApplied = () => {
-    if (selectedTypes.length || selectedTimes !== DEFAULT_TIMES || selectedTags.length || selectedLanguage !== SELECTED_LANGUAGE || selectedGroup !== PUBLIC_GROUP) {
-      return true
+  if (!groupsProp || groupsProp === undefined) {
+    const resetFilters = () => {
+      setSelectedTypes([])
+      setSelectedTimes(DEFAULT_TIMES)
+      setSelectedTags([])
+      setSelectedLanguage(SELECTED_LANGUAGE)
+      setSelectedGroup(PUBLIC_GROUP)
     }
-
-    return false
-  }
-
-  // const { loading: groupsLoading } = useGroups()
-
-  const loading = phenomenaTypesLoading || groupsLoading
-
-  const allGroups = groups?.map( g => {
-    return g.value
-  })
-  // const bc = filter(groups, group => group.label).sort( (a,b) => {
-  //   if (a.label < b.label) {
-  //     return -1;
-  //   }
-  //   if (a.label > b.label) {
-  //     return 1;
-  //   }
   
-  //   // names must be equal
-  //   return 0;
-  // }).map(g => {
-  //   return g.value
-  // })
-  const ALL_GROUP = { value: passedGroups || concat([PUBLIC_GROUP.value], allGroups), label: requestTranslation('allWord')}
-
-    const sortedGroups = filter(groups, group => group.label).sort( (a,b) => {
-      if (a.label < b.label) {
-        return -1;
+    const areFiltersApplied = () => {
+      if (selectedTypes.length || selectedTimes !== DEFAULT_TIMES || selectedTags.length || selectedLanguage !== SELECTED_LANGUAGE || selectedGroup !== PUBLIC_GROUP) {
+        return true
       }
-      if (a.label > b.label) {
-        return 1;
-      }
-      return 0;
-    })
-  const groupOptions = passedGroups || concat([ALL_GROUP, PUBLIC_GROUP], sortedGroups)
-
-  const [typesShown, setTypesShown] = useState(false)
-  const [timesShown, setTimesShown] = useState(false)
-  const [tagsShown, setTagsShown] = useState(false)
-  const [groupsShown, setGroupsShown] = useState(false)
-  const [languagesShown, setLanguagesShown] = useState(false)
-
-  const [selectedTypes, setSelectedTypes] = useState([])
-  const [selectedTimes, setSelectedTimes] = useState(DEFAULT_TIMES)
-  const [selectedTags, setSelectedTags] = useState([])
-  const [selectedLanguage, setSelectedLanguage] = useState(SELECTED_LANGUAGE)
-  const [selectedGroup, setSelectedGroup] = useState(ALL_GROUP)
-  const [debouncedSearchValue, clearTimeout] = useDebounce(search, SEARCH_DEBOUNCE_MS)
-
-  const {
-    loading: phenomenaTypesLoading,
-    phenomenonTypes,
-    phenomenonTypesById
-  } = usePhenomenonTypes(selectedGroup.value)
-
   
-
-  useEffect(() => {
-    onFilterChange({
-      types: selectedTypes,
-      times: selectedTimes,
-      tags: selectedTags,
-      language: selectedLanguage,
-      group: selectedGroup,
+      return false
+    }
+  
+    const [typesShown, setTypesShown] = useState(false)
+    const [timesShown, setTimesShown] = useState(false)
+    const [tagsShown, setTagsShown] = useState(false)
+    const [groupsShown, setGroupsShown] = useState(false)
+    const [languagesShown, setLanguagesShown] = useState(false)
+  
+    const [selectedTypes, setSelectedTypes] = useState([])
+    const [selectedTimes, setSelectedTimes] = useState(DEFAULT_TIMES)
+    const [selectedTags, setSelectedTags] = useState([])
+    const [selectedLanguage, setSelectedLanguage] = useState(SELECTED_LANGUAGE)
+    const [selectedGroup, setSelectedGroup] = useState(PUBLIC_GROUP)
+    const [debouncedSearchValue, clearTimeout] = useDebounce(search, SEARCH_DEBOUNCE_MS)
+  
+    const {
+      loading: phenomenaTypesLoading,
+      phenomenonTypes,
+      phenomenonTypesById
+    } = usePhenomenonTypes(selectedGroup.value)
+  
+    const { loading: groupsLoading, groups } = useGroups()
+  
+    const loading = phenomenaTypesLoading || groupsLoading
+    const groupOptions = passedGroups || concat([PUBLIC_GROUP], filter(groups, group => group.id))
+  
+    useEffect(() => {
+      onFilterChange({
+        types: selectedTypes,
+        times: selectedTimes,
+        tags: selectedTags,
+        language: selectedLanguage,
+        group: selectedGroup,
+        page,
+        search: debouncedSearchValue,
+        filtersActive: areFiltersApplied()
+      })
+    }, [
+      selectedTypes,
+      selectedTimes,
+      selectedTags,
+      selectedLanguage,
+      selectedGroup,
       page,
-      search: debouncedSearchValue,
-      filtersActive: areFiltersApplied()
-    })
-  }, [
-    selectedTypes,
-    selectedTimes,
-    selectedTags,
-    selectedLanguage,
-    selectedGroup,
-    page,
-    debouncedSearchValue
-  ])
-
-  useEffect(() => {
-    if (manualFilterReset) {
-      resetFilters()
+      debouncedSearchValue
+    ])
+  
+    useEffect(() => {
+      if (manualFilterReset) {
+        resetFilters()
+      }
+    }, [manualFilterReset])
+  
+    return (
+      <Fragment>
+        {loading && <div className="py-2 pl-2">{requestTranslation('loading')}</div>}
+        <div className='mb-3'>
+            <OptionDropdown
+                label={requestTranslation('createPhenomenaFormTypeLabel')}
+                optionsShown={typesShown}
+                type={'type'}
+                title={selectedTypes.length ? getTypeLabel(selectedTypes) : requestTranslation('all')}
+                selectedOption={selectedTypes}
+                handleOptionSelect={type => setSelectedTypes(addOrRemoveValueFromArray(selectedTypes, type))}
+                options={map(phenomenonTypes, type => ({
+                  value: type.id,
+                  label: type.alias || type.title || type.label,
+                  style: type.style
+                }))}
+                onTabClick={() => setTypesShown(!typesShown)}
+                resetFilters={() => setSelectedTypes([])}
+                countShown={countShown}
+            />
+        </div>
+        <div className='mb-3'>
+            <TimelineOptionDropdown
+                label={requestTranslation('time')}
+                optionsShown={timesShown}
+                title={`${selectedTimes.min || ''} - ${selectedTimes.max || ''}`}
+                selectedOption={selectedTimes}
+                handleOptionSelect={times => setSelectedTimes(times)}
+                onTabClick={() => setTimesShown(!timesShown)}
+                countShown={countShown}
+            />
+        </div>
+        <div className='mb-3'>
+            <TagOptionDropdown
+                label={requestTranslation('tags')}
+                optionsShown={tagsShown}
+                title={selectedTags.length === 0 ? requestTranslation('none') : getTagLabel(selectedTags, selectedLanguage)}
+                selectedOption={selectedTags}
+                handleOptionSelect={tag => setSelectedTags(addOrRemoveValueFromArray(selectedTags, tag))}
+                onTabClick={() => setTagsShown(!tagsShown)}
+                group={selectedGroup.value}
+                language={selectedLanguage.value}
+                countShown={countShown}
+                useTags={useTags}
+            />
+        </div>
+        <div className='mb-3'>
+            <OptionDropdown
+                label={requestTranslation('group')}
+                title={selectedGroup.label}
+                type={'radio'}
+                optionsShown={groupsShown}
+                options={groupOptions}
+                selectedOption={selectedGroup}
+                onTabClick={() => setGroupsShown(!groupsShown)}
+                handleOptionSelect={e =>  setSelectedGroup(find(groupOptions, { label: e.target.innerText }))}
+                countShown={countShown}
+            />
+        </div>
+        <div className='mb-3'>
+            <OptionDropdown
+                label={requestTranslation('language')}
+                title={selectedLanguage.label}
+                type={'radio'}
+                optionsShown={languagesShown}
+                options={radarLanguagesWithAll()}
+                selectedOption={selectedLanguage}
+                handleOptionSelect={e => setSelectedLanguage(find(radarLanguagesWithAll(), { label: e.target.innerText }))}
+                onTabClick={() => setLanguagesShown(!languagesShown)}
+                countShown={countShown}
+            />
+        </div>
+        {resetShown && (
+          <button className='btn btn-outline-secondary w-100 d-flex align-items-center justify-content-center' onClick={resetFilters}>
+              <i className='material-icons mr-1' style={{ fontSize: '16px' }}>replay</i>
+              {requestTranslation('resetFilters')}
+          </button>
+        )}
+      </Fragment>
+    )
+  }
+  else {
+    const resetFilters = () => {
+      setSelectedTypes([])
+      setSelectedTimes(DEFAULT_TIMES)
+      setSelectedTags([])
+      setSelectedLanguage(SELECTED_LANGUAGE)
+      setSelectedGroup(ALL_GROUP)
     }
-  }, [manualFilterReset])
-
-  return (
-    <Fragment>
-      {loading && <div className="py-2 pl-2">{requestTranslation('loading')}</div>}
-      <div className='mb-3'>
-          <OptionDropdown
-              label={requestTranslation('createPhenomenaFormTypeLabel')}
-              optionsShown={typesShown}
-              type={'type'}
-              title={selectedTypes.length ? getTypeLabel(selectedTypes) : requestTranslation('all')}
-              selectedOption={selectedTypes}
-              handleOptionSelect={type => setSelectedTypes(addOrRemoveValueFromArray(selectedTypes, type))}
-              options={map(phenomenonTypes, type => ({
-                value: type.id,
-                label: type.alias || type.title || type.label,
-                style: type.style
-              }))}
-              onTabClick={() => setTypesShown(!typesShown)}
-              resetFilters={() => setSelectedTypes([])}
-              countShown={countShown}
-          />
-      </div>
-      <div className='mb-3'>
-          <TimelineOptionDropdown
-              label={requestTranslation('time')}
-              optionsShown={timesShown}
-              title={`${selectedTimes.min || ''} - ${selectedTimes.max || ''}`}
-              selectedOption={selectedTimes}
-              handleOptionSelect={times => setSelectedTimes(times)}
-              onTabClick={() => setTimesShown(!timesShown)}
-              countShown={countShown}
-          />
-      </div>
-      <div className='mb-3'>
-          <TagOptionDropdown
-              label={requestTranslation('tags')}
-              optionsShown={tagsShown}
-              title={selectedTags.length === 0 ? requestTranslation('none') : getTagLabel(selectedTags, selectedLanguage)}
-              selectedOption={selectedTags}
-              handleOptionSelect={tag => setSelectedTags(addOrRemoveValueFromArray(selectedTags, tag))}
-              onTabClick={() => setTagsShown(!tagsShown)}
-              group={selectedGroup.value}
-              language={selectedLanguage.value}
-              countShown={countShown}
-              useTags={useTags}
-          />
-      </div>
-      <div className='mb-3'>
-          <OptionDropdown
-              label={requestTranslation('group')}
-              title={selectedGroup.label}
-              type={'radio'}
-              optionsShown={groupsShown}
-              options={groupOptions}
-              selectedOption={selectedGroup}
-              onTabClick={() => setGroupsShown(!groupsShown)}
-              handleOptionSelect={e =>  setSelectedGroup(find(groupOptions, { label: e.target.innerText }))}
-              countShown={countShown}
-          />
-      </div>
-      <div className='mb-3'>
-          <OptionDropdown
-              label={requestTranslation('language')}
-              title={selectedLanguage.label}
-              type={'radio'}
-              optionsShown={languagesShown}
-              options={radarLanguagesWithAll()}
-              selectedOption={selectedLanguage}
-              handleOptionSelect={e => setSelectedLanguage(find(radarLanguagesWithAll(), { label: e.target.innerText }))}
-              onTabClick={() => setLanguagesShown(!languagesShown)}
-              countShown={countShown}
-          />
-      </div>
-      {resetShown && (
-        <button className='btn btn-outline-secondary w-100 d-flex align-items-center justify-content-center' onClick={resetFilters}>
-            <i className='material-icons mr-1' style={{ fontSize: '16px' }}>replay</i>
-            {requestTranslation('resetFilters')}
-        </button>
-      )}
-    </Fragment>
-  )
+  
+    const areFiltersApplied = () => {
+      if (selectedTypes.length || selectedTimes !== DEFAULT_TIMES || selectedTags.length || selectedLanguage !== SELECTED_LANGUAGE || selectedGroup !== PUBLIC_GROUP) {
+        return true
+      }
+  
+      return false
+    }
+  
+    // const { loading: groupsLoading } = useGroups()
+  
+    const loading = phenomenaTypesLoading || groupsLoading
+  
+    const allGroups = groupsProp?.map( g => {
+      return g.value
+    })
+    // const bc = filter(groups, group => group.label).sort( (a,b) => {
+    //   if (a.label < b.label) {
+    //     return -1;
+    //   }
+    //   if (a.label > b.label) {
+    //     return 1;
+    //   }
+    
+    //   // names must be equal
+    //   return 0;
+    // }).map(g => {
+    //   return g.value
+    // })
+    const ALL_GROUP = { value: passedGroups || concat([PUBLIC_GROUP.value], allGroups), label: requestTranslation('allWord')}
+  
+      const sortedGroups = filter(groupsProp, group => group.label).sort( (a,b) => {
+        if (a.label < b.label) {
+          return -1;
+        }
+        if (a.label > b.label) {
+          return 1;
+        }
+        return 0;
+      })
+    const groupOptions = passedGroups || concat([ALL_GROUP, PUBLIC_GROUP], sortedGroups)
+  
+    const [typesShown, setTypesShown] = useState(false)
+    const [timesShown, setTimesShown] = useState(false)
+    const [tagsShown, setTagsShown] = useState(false)
+    const [groupsShown, setGroupsShown] = useState(false)
+    const [languagesShown, setLanguagesShown] = useState(false)
+  
+    const [selectedTypes, setSelectedTypes] = useState([])
+    const [selectedTimes, setSelectedTimes] = useState(DEFAULT_TIMES)
+    const [selectedTags, setSelectedTags] = useState([])
+    const [selectedLanguage, setSelectedLanguage] = useState(SELECTED_LANGUAGE)
+    const [selectedGroup, setSelectedGroup] = useState(ALL_GROUP)
+    const [debouncedSearchValue, clearTimeout] = useDebounce(search, SEARCH_DEBOUNCE_MS)
+  
+    const {
+      loading: phenomenaTypesLoading,
+      phenomenonTypes,
+      phenomenonTypesById
+    } = usePhenomenonTypes(selectedGroup.value)
+  
+    
+  
+    useEffect(() => {
+      onFilterChange({
+        types: selectedTypes,
+        times: selectedTimes,
+        tags: selectedTags,
+        language: selectedLanguage,
+        group: selectedGroup,
+        page,
+        search: debouncedSearchValue,
+        filtersActive: areFiltersApplied()
+      })
+    }, [
+      selectedTypes,
+      selectedTimes,
+      selectedTags,
+      selectedLanguage,
+      selectedGroup,
+      page,
+      debouncedSearchValue
+    ])
+  
+    useEffect(() => {
+      if (manualFilterReset) {
+        resetFilters()
+      }
+    }, [manualFilterReset])
+  
+    return (
+      <Fragment>
+        {loading && <div className="py-2 pl-2">{requestTranslation('loading')}</div>}
+        <div className='mb-3'>
+            <OptionDropdown
+                label={requestTranslation('createPhenomenaFormTypeLabel')}
+                optionsShown={typesShown}
+                type={'type'}
+                title={selectedTypes.length ? getTypeLabel(selectedTypes) : requestTranslation('all')}
+                selectedOption={selectedTypes}
+                handleOptionSelect={type => setSelectedTypes(addOrRemoveValueFromArray(selectedTypes, type))}
+                options={map(phenomenonTypes, type => ({
+                  value: type.id,
+                  label: type.alias || type.title || type.label,
+                  style: type.style
+                }))}
+                onTabClick={() => setTypesShown(!typesShown)}
+                resetFilters={() => setSelectedTypes([])}
+                countShown={countShown}
+            />
+        </div>
+        <div className='mb-3'>
+            <TimelineOptionDropdown
+                label={requestTranslation('time')}
+                optionsShown={timesShown}
+                title={`${selectedTimes.min || ''} - ${selectedTimes.max || ''}`}
+                selectedOption={selectedTimes}
+                handleOptionSelect={times => setSelectedTimes(times)}
+                onTabClick={() => setTimesShown(!timesShown)}
+                countShown={countShown}
+            />
+        </div>
+        <div className='mb-3'>
+            <TagOptionDropdown
+                label={requestTranslation('tags')}
+                optionsShown={tagsShown}
+                title={selectedTags.length === 0 ? requestTranslation('none') : getTagLabel(selectedTags, selectedLanguage)}
+                selectedOption={selectedTags}
+                handleOptionSelect={tag => setSelectedTags(addOrRemoveValueFromArray(selectedTags, tag))}
+                onTabClick={() => setTagsShown(!tagsShown)}
+                group={selectedGroup.value}
+                language={selectedLanguage.value}
+                countShown={countShown}
+                useTags={useTags}
+            />
+        </div>
+        <div className='mb-3'>
+            <OptionDropdown
+                label={requestTranslation('group')}
+                title={selectedGroup.label}
+                type={'radio'}
+                optionsShown={groupsShown}
+                options={groupOptions}
+                selectedOption={selectedGroup}
+                onTabClick={() => setGroupsShown(!groupsShown)}
+                handleOptionSelect={e =>  setSelectedGroup(find(groupOptions, { label: e.target.innerText }))}
+                countShown={countShown}
+            />
+        </div>
+        <div className='mb-3'>
+            <OptionDropdown
+                label={requestTranslation('language')}
+                title={selectedLanguage.label}
+                type={'radio'}
+                optionsShown={languagesShown}
+                options={radarLanguagesWithAll()}
+                selectedOption={selectedLanguage}
+                handleOptionSelect={e => setSelectedLanguage(find(radarLanguagesWithAll(), { label: e.target.innerText }))}
+                onTabClick={() => setLanguagesShown(!languagesShown)}
+                countShown={countShown}
+            />
+        </div>
+        {resetShown && (
+          <button className='btn btn-outline-secondary w-100 d-flex align-items-center justify-content-center' onClick={resetFilters}>
+              <i className='material-icons mr-1' style={{ fontSize: '16px' }}>replay</i>
+              {requestTranslation('resetFilters')}
+          </button>
+        )}
+      </Fragment>
+    )
+  }
 }
 
 export default ContentFilters
