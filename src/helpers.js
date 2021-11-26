2a93b21b41bfa05fd04787fa8e0f2e8cf6dba427
Phan <phan.duc@futuresplatform.com>
2021-11-27 01:52:11 +0200

    debug3

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,14 @@
 import { find, capitalize, isString } from 'lodash-es'
 import { requestTranslation } from '@sangre-fp/i18n'
 
-export const addOrRemoveValueFromArray = (array, value) => find(array, value) ? _.filter(array, v => v.label !== value.label) : [...array, value]
+export const addOrRemoveValueFromArray = (array, value) => find(array, value) ? _.filter(array, v => (
+    (v.label !== value.label)
+     && ((value?.label?.fi && v?.label?.fi) ? value?.label?.fi !== v?.label?.fi :  null)
+     && ((value?.label?.en && v?.label?.en) ? value?.label?.en !== v?.label?.en :  null)
+     && ((value?.label?.fi && typeof v?.label === 'string') ? value?.label?.fi !== v?.label :  null)
+     && ((value?.label?.en && typeof v?.label === 'string') ? value?.label?.en !== v?.label :  null)
+     && ((typeof value?.label  === 'string' && v?.label === 'object') ? (value?.label !== v?.label?.fi && value?.label !== v?.label?.en) :  null)
+)) : [...array, value]
 
 export const getTypeLabel = selectedTypes => selectedTypes.map(({ label }) => capitalize(requestTranslation(label) || label)).join(', ')
 
