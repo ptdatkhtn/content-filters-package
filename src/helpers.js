524de6d68d9effb001d03da1054063d082642fd7
Phan <phan.duc@futuresplatform.com>
2021-11-27 02:08:19 +0200

    debug4

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,23 @@
 import { find, capitalize, isString } from 'lodash-es'
 import { requestTranslation } from '@sangre-fp/i18n'
 
-export const addOrRemoveValueFromArray = (array, value) => find(array, value) ? _.filter(array, v => (
-    (v.label !== value.label)
-     && ((value?.label?.fi && v?.label?.fi) ? value?.label?.fi !== v?.label?.fi :  null)
-     && ((value?.label?.en && v?.label?.en) ? value?.label?.en !== v?.label?.en :  null)
-     && ((value?.label?.fi && typeof v?.label === 'string') ? value?.label?.fi !== v?.label :  null)
-     && ((value?.label?.en && typeof v?.label === 'string') ? value?.label?.en !== v?.label :  null)
-     && ((typeof value?.label  === 'string' && v?.label === 'object') ? (value?.label !== v?.label?.fi && value?.label !== v?.label?.en) :  null)
-)) : [...array, value]
+export const addOrRemoveValueFromArray = (array, value) => find(array, value) ? _.filter(array, v => {
+    if (typeof v?.label === 'string' && typeof value?.label  === 'string') {
+        return (v.label !== value.label)
+    } else if (typeof value?.label  === 'object' && typeof v?.label  === 'object') {
+        return (JSON.stringify(value?.label) !== JSON.stringify(v?.label) )
+    } else if (typeof value?.label  === 'object' && typeof v?.label  === 'string') {
+        return (v?.label !== value?.label?.fi && v?.label !== value?.label?.en)
+    } else if (typeof v?.label  === 'object' && typeof value?.label  === 'string') {
+        if (!!v?.label?.fi) {
+            return (value?.label !== v?.label?.fi)
+        } else if (!!v?.label?.en) {
+            return (value?.label !== v?.label?.en)
+        }
+    } else {
+        return (JSON.stringify(value?.label) !== JSON.stringify(v?.label) )
+    }
+}) : [...array, value]
 
 export const getTypeLabel = selectedTypes => selectedTypes.map(({ label }) => capitalize(requestTranslation(label) || label)).join(', ')
 
